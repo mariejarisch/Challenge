f881e443447a2594462a2392955c3f79e0788965
mariejarisch <marie.jarisch@student.hpi.uni-potsdam.de>
2026-01-28 16:38:01 +0100

    add unused model, gemini integration and basic chat functionality

--- challenge/myapp/views.py
+++ challenge/myapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import google.generativeai as genai
+from django.conf import settings
+import json
+
+genai.configure(api_key=settings.GOOGLE_API_KEY)
+print("Gemini API Key:", settings.GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat(history=[])
+
+@csrf_exempt
+def input_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        user_message = data.get("message")
+        response = chat.send_message(user_message)
+        bot_reply = response.text
+        return JsonResponse({"reply": bot_reply})
+    return render(request, "chat.html")
